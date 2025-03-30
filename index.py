ef506da7a7b2bb5cde7236e849e89da711ea6503
wandera-emmanuel <emmanuelbahati902@gmail.com>
2025-03-30 12:37:12 +0300

    python week 2 work

---
+++ index.py
@@ -0,0 +1,13 @@
+my_list=[]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.pop()
+my_list.sort()
+index_of_30 = my_list.index(30)
+print(f"The index of 30 is: {index_of_30}")
+
+
